0de38eb3500c4523ec18ba0d8be704d62affd7f2
Andrii Maliuta <quadr988@gmail.com>
2023-03-24 19:11:13 +0200

    attachments.rs

--- src/jira/attachments.rs
+++ src/jira/attachments.rs
@@ -1,19 +1,37 @@
 pub mod attach {
+    use reqwest::Response;
+
     pub struct IssueAttachments();
 
     impl IssueAttachments {
-        // Get attachment
-        // GET /rest/api/2/attachment/{id}
-        // Remove attachment
-        // DELETE /rest/api/2/attachment/{id}
         // GET /rest/api/2/attachment/{id}/expand/human
         // GET /rest/api/2/attachment/{id}/expand/raw
-        // Get attachment meta
-        // GET /rest/api/2/attachment/meta
 
-        pub async fn get_attachment() {
-
+        pub async fn get_attachment(&self, url: &str, token: String, id: String) -> String {
+            let req_url = format!("{url}/rest/api/2/attachment/{id}");
+            let client = reqwest::Client::new();
+            let resp: Response = client.get(&req_url)
+                .header("Authorization", format!("Basic {token}"))
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .send().await.unwrap();
+            let body = resp.text().await.unwrap();
+            return body;
         }
 
+        pub async fn delete_attachment(&self, url: &str, token: String, id: String) -> String {
+            let req_url = format!("{url}/rest/api/2/attachment/{id}");
+            let client = reqwest::Client::new();
+            let resp: Response = client.delete(&req_url)
+                .header("Authorization", format!("Basic {token}"))
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .send().await.unwrap();
+            let body = resp.text().await.unwrap();
+            return body;
+        }
+
+        // Get attachment meta
+        // GET /rest/api/2/attachment/meta
     }
 }
--- src/jira/props.rs
+++ src/jira/props.rs
@@ -2,14 +2,16 @@ pub mod props {
     pub struct PropsService();
 
     impl PropsService {
+        // todo
         //GET /rest/api/2/application-properties
         pub async fn get_prop(&self, url: String, token: String) {
 
         }
 
+        // todo
         //PUT /rest/api/2/application-properties/{id}
         pub async fn set_prop(&self, url: String, token: String, id: String) {
-            
+
         }
     }
 }
